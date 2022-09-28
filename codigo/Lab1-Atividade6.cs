99d4692d364956b5c20cf7d886c12c00dec2a89b
Caio Vinícius Braga <111787841+caiobraga11122002@users.noreply.github.com>
2022-09-27 23:10:04 -0300

    Create Lab1-Atividade6.cs

---
+++ codigo/Lab1-Atividade6.cs
@@ -0,0 +1,39 @@
+using System;
+
+class Program
+{
+    static void Elevado(int x, int y)
+    {
+
+        int ValY = y;
+
+        int resultado = x;
+
+        do
+        {
+
+            resultado *= x;
+            y--;
+
+        } while (y != 1);
+
+        Console.Write($"Resultado de {x} ^ {ValY} = {resultado}");
+
+    }
+
+    public static void Main(string[] args)
+    {
+
+        int X, Y;
+
+        Console.Write("Qual o valor de X: ");
+        X = int.Parse(Console.ReadLine());
+
+        Console.Write("Qual o valor de y: ");
+        Y = int.Parse(Console.ReadLine());
+
+        Elevado(X, Y);
+
+        Console.ReadKey();
+    }
+}
