be9f2c1ff04c3a8357125f9a27c5883011099ad7
Caio Vinícius Braga <111787841+caiobraga11122002@users.noreply.github.com>
2022-10-06 23:12:26 -0300

    Update Lab2.cs

--- codigo/Lab2.cs
+++ codigo/Lab2.cs
@@ -1,1 +1,35 @@
+Atividade 1-
 
+using System;
+
+class Program
+{
+    static int FatrialRecursivo(int N)
+    {
+        if (N <= 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return N * FatrialRecursivo(N - 1);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+
+        int N, Resultado;
+
+        Console.Write("Qual o valor de N: ");
+        N = int.Parse(Console.ReadLine());
+
+        Resultado = FatrialRecursivo(N);
+        Console.Write($"Resultado: {Resultado} ");
+
+        Console.ReadKey();
+    }
+}
+
+
+Atividade 2-
