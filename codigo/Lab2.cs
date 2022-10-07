19c850362efaaf090fd3362447139675423602fd
Caio Vinícius Braga <111787841+caiobraga11122002@users.noreply.github.com>
2022-10-06 23:12:59 -0300

    Update Lab2.cs

--- codigo/Lab2.cs
+++ codigo/Lab2.cs
@@ -33,3 +33,49 @@ class Program
 
 
 Atividade 2-
+    
+    using System;
+
+class Program
+{
+    static int MultiplicacaoRecursiva(int N, int N2)
+    {
+        int Resultado = 0;
+
+        if (N == 0 || N2 == 0)
+        {
+            return 1;
+        }
+
+        if (N2 >= 0)
+        {
+            return Resultado = N + MultiplicacaoRecursiva(N, N2--);
+        }
+        else
+        {
+            return Resultado;
+        }
+
+    }
+
+    public static void Main(string[] args)
+    {
+
+        int N, N2, Resultado;
+
+        Console.Write("Qual o valor de N: ");
+        N = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("X");
+
+        Console.Write("Qual o valor de N2: ");
+        N2 = int.Parse(Console.ReadLine());
+
+
+        Resultado = MultiplicacaoRecursiva(N, N2);
+
+        Console.Write($"Resultado: {Resultado} ");
+
+        Console.ReadKey();
+    }
+}
