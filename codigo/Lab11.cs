07a0a19d6e437b5b7df4d72e4633e295d3252efc
Caio Vinícius Braga <111787841+caiobraga11122002@users.noreply.github.com>
2022-11-17 00:19:57 -0300

    Update Lab11.cs

--- codigo/Lab11.cs
+++ codigo/Lab11.cs
@@ -1,4 +1,5 @@
-using System;
+//.....................................................................................................................FILA
+using System;//CLASSE FILA
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -49,7 +50,7 @@ namespace ProjetoClasse
     }
 }
 
-using System;
+using System;//MAIN FILA
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -105,40 +106,121 @@ namespace ProjetoClasse
         }
     }
 }
-<Project Sdk="Microsoft.NET.Sdk">
 
-  <PropertyGroup>
-    <OutputType>Exe</OutputType>
-    <TargetFramework>net6.0</TargetFramework>
-    <ImplicitUsings>enable</ImplicitUsings>
-    <Nullable>enable</Nullable>
-  </PropertyGroup>
+//.....................................................................................................................PILHA
 
-</Project>
+using System;//CLASSE PILHA
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoClasse
+{
+    internal class Pilha
+    {
+        const int Capacidade = 5;
+        private int quantidade = 0;
+        private int inicio = 0;
+        private int fim = 0;
+        private string[] dados = new string[Capacidade];
+
+        public int tamanho()
+        {
+            return quantidade;
+        }
+
+        public void inserirElemento(string valor_i)
+        {
+            if (tamanho() == Capacidade)
+            {
+                throw new Exception("Pilha cheia");
+            }
+            else
+            {
+                dados[inicio] = valor_i;
+                inicio = (inicio + 1) % Capacidade;
+                quantidade++;
+            }
+        }
+        public string RemoveElemento()
+        {
+            if (tamanho() == 0)
+            {
+                throw new Exception("Pilha Vazia");
+            }
+            else
+            {
+                String valor = dados[fim];
+                fim = (fim + 1) % Capacidade;
+                quantidade--;
+                return valor;
+            }
+        }
+    }
+}
 
 
-Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio Version 17
-VisualStudioVersion = 17.3.32825.248
-MinimumVisualStudioVersion = 10.0.40219.1
-Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "ProjetoClasse", "ProjetoClasse.csproj", "{EBE5FC88-CAF2-4EE5-80FD-AB4B0443FE28}"
-EndProject
-Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Debug|Any CPU = Debug|Any CPU
-		Release|Any CPU = Release|Any CPU
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{EBE5FC88-CAF2-4EE5-80FD-AB4B0443FE28}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
-		{EBE5FC88-CAF2-4EE5-80FD-AB4B0443FE28}.Debug|Any CPU.Build.0 = Debug|Any CPU
-		{EBE5FC88-CAF2-4EE5-80FD-AB4B0443FE28}.Release|Any CPU.ActiveCfg = Release|Any CPU
-		{EBE5FC88-CAF2-4EE5-80FD-AB4B0443FE28}.Release|Any CPU.Build.0 = Release|Any CPU
-	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
-	GlobalSection(ExtensibilityGlobals) = postSolution
-		SolutionGuid = {97A5F49F-19EC-49BD-89E5-51493A968B3B}
-	EndGlobalSection
-EndGlobal
+using System;//MAIN PILHA
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoClasse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string op = "", valor;
+
+            Pilha minhaPilha = new Pilha();
+            Console.WriteLine("Sistemas de Fila");
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine("\n1 -> inserir \n2 -> remover \n3 -> mostrar");
+                    op = Console.ReadLine();
+
+                    switch (op)
+                    {
+                        case "1":
+                            Console.Write("Qual o elemento para inserir na fila: ");
+                            valor = Console.ReadLine();
+                            minhaPilha.inserirElemento(valor);
+                            break;
+
+                        case "2":
+                            Console.Write("Qual o elemento para Remover na fila: ");
+                            valor = Console.ReadLine();
+                            minhaPilha.RemoveElemento();
+                            break;
+
+                        case "3":
+                            Console.Write("Quantidade total de elementos: ");
+                            minhaPilha.tamanho();
+                            break;
+
+                        default:
+                            Console.WriteLine("Opção inválida!!");
+                            break;
+                    }
+                }
+                catch (Exception erro)
+                {
+                    Console.WriteLine(erro.Message);
+                }
+            } while (op != "9");
+        }
+    }
+}
+
+//.....................................................................................................................LISTA
+
+
+
+
 
