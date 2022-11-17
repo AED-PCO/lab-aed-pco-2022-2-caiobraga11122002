9929e5115698d07fd792c9682c1e9c9fc4e1abac
Caio Vinícius Braga <111787841+caiobraga11122002@users.noreply.github.com>
2022-11-17 18:22:29 -0300

    Update Lab10.cs

--- codigo/Lab10.cs
+++ codigo/Lab10.cs
@@ -217,3 +217,109 @@ namespace ProjetoClasse
 }
 
 //.....................................................................................................................LISTA
+
+using System;//CLASS LISTA
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoClasse
+{
+    internal class Lista
+    {
+        const int Capacidade = 5;
+        private int quantidade = 0;
+        private int inicio = 0;
+        private int fim = 0;
+        private string[] dados = new string[Capacidade];
+
+        public int tamanho()
+        {
+            Console.WriteLine(quantidade);
+            return quantidade;
+        }
+
+        public void inserirElemento(string valor_i, int pos)
+        {
+            if (tamanho() == Capacidade)
+            {
+                throw new Exception("Pilha cheia");
+            }
+            else
+            {
+                dados[inicio] = valor_i;
+                inicio = (inicio + 1) % Capacidade;
+                quantidade++;
+                pos++;
+            }
+        }
+        public string RemoveElemento(int pos)
+        {
+            if (tamanho() == 0)
+            {
+                throw new Exception("Pilha Vazia");
+            }
+            else if (pos == quantidade);
+            {
+                String valor = dados[fim];
+                fim = (fim + 1) % Capacidade;
+                quantidade--;
+                return valor;
+            }
+        }
+    }
+}
+
+
+namespace ProjetoClasse//MAIN LISTA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Opcao, Valor; 
+                int pos;
+
+            Lista Lista_Aed = new Lista();
+            do
+            {
+                Console.WriteLine("\n1 - Inserir \n2 - Remover \n3 - Qantidade");
+                Console.WriteLine("");
+
+                Opcao = Console.ReadLine();
+
+                switch (Opcao)
+                {
+                    case "1":
+                        Console.Write("Qual o elemento para inserir na fila: ");
+                        Valor = Console.ReadLine();
+                        Console.Write("Qual a posição: ");
+                        pos = int.Parse(Console.ReadLine());
+                        Lista_Aed.inserirElemento(Valor, pos);
+                        break;
+
+                    case "2":
+                        Console.Write("Qual o elemento para Remover na fila: ");
+                        Valor = Console.ReadLine();
+                        Console.Write("Qual a posição: ");
+                        pos = int.Parse(Console.ReadLine());
+                        Lista_Aed.RemoveElemento(pos);
+                        break;
+
+                    case "3":
+                        Console.Write("Quantidade total de elementos: ");
+                        Lista_Aed.tamanho();
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+
+                {
+                }
+            } while (Opcao != "9");
+        }
+    }
+}
