c3e8cd50acfcde91e12609b8c4a9b846e811ce75
Caio Vinícius Braga <111787841+caiobraga11122002@users.noreply.github.com>
2022-11-24 15:11:54 -0300

    Update Lab10.cs

--- codigo/Lab10.cs
+++ codigo/Lab10.cs
@@ -283,3 +283,161 @@ namespace PosicaoPilha
         }
     }
 }
+
+//.......................................................................................................................MAIN LISTA
+
+using PosicaoLista;//MAIN
+using ListaClasse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassePilha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Op;
+            int Val;
+            Posicao Pos = new Posicao();
+
+            do
+            {
+                Console.Write("Programa de Lista:\n 1 -> Inserir\n 2 -> Remover\n 3 -> Mostrar\n");
+
+
+                Op = Console.ReadLine();
+
+                switch (Op)
+                {
+                    case "1":
+                        Console.Write("Qual será inserido: ");
+                        Val = int.Parse(Console.ReadLine());
+                        Lista.Inserir(Val);
+                        break;
+                    case "2":
+                        Lista.Remover();
+                        Console.ReadKey();
+                        break;
+                    case "3":
+                        Lista.mostrar();
+                        Console.ReadKey();
+                        break;
+                }
+                Console.Clear();
+            } while (Op != "9");
+        }
+    }
+}
+
+//.......................................................................................................................CLASSE LISTA
+
+using PosicaoLista;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaClasse
+{
+    internal class Lista
+    {
+        public static Posicao Inicio = new Posicao();
+        public static Posicao Fim = Inicio;
+        Posicao PosicaoPilha = new Posicao();
+
+        public Lista()
+        {
+            Inicio = new Posicao();
+            Fim = Inicio;
+        }
+
+        public static void Inserir(int Numero)
+        {
+            Posicao NovaPosicao = new Posicao();
+            NovaPosicao.Elemento = Numero;
+
+            Fim.Ponteiro = NovaPosicao;
+            Fim = NovaPosicao;
+        }
+
+        public static void mostrar()
+        {
+            Posicao x = Inicio.Ponteiro;
+
+            Console.Write("Sua Lista:");
+            do
+            {
+                Console.Write(x.Elemento + " ");
+                x = x.Ponteiro;
+
+            } while (x != null);
+        }
+        public static void Remover()
+        {
+            int RemoveLista;
+
+            if (Inicio == Fim)
+            {
+                Console.Write("Vazio");
+            }
+
+           else
+            {
+                Posicao In = Inicio;
+
+                Console.Write("Qual o elemento deseja retirar da Lista: ");
+                RemoveLista = int.Parse(Console.ReadLine());
+
+                while (In.Ponteiro != Fim)
+                {
+                    if(In.Ponteiro.Elemento == RemoveLista)
+                    {
+                        In.Ponteiro = In.Ponteiro.Ponteiro;
+                    }
+                    else
+                    {
+                        In = In.Ponteiro;
+                    }
+                }
+                Console.Write(RemoveLista+" Removido");
+
+            }
+        }
+    }
+}
+
+//.......................................................................................................................CLASSE PONTEIRO
+
+using System;
+using ListaClasse;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PosicaoLista
+{
+    internal class Posicao
+    {
+        public int Elemento;
+        public Posicao Ponteiro;
+
+        public Posicao()
+        {
+            this.Elemento = 0;
+        }
+
+        public Posicao(int elemento)
+        {
+            this.Elemento = elemento;
+        }
+    }
+}
+
+//.......................................................................................................................CALCULADORA 
